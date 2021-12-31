b2675755f96a5b17c15907a011715c1ecf797493
a7sharma <94664895+a7sharma@users.noreply.github.com>
2021-12-31 14:03:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleHide(){
+let btn = document.getElementById('btn');
+let card = document.getElementById('card'); 
+if(card.style.display != 'none'){
+card.style.display = 'none';
+}
+else{
+card.style.display = 'block';
+}
+}
